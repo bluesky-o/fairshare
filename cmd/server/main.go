c75cdd2e0b6f50dbd53f91525202e6b71c0c948e
bluesky-o <blueskyco005@gmail.com>
2026-05-15 23:19:42 +0530

    initial server setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/bluesky-o/fairshare/internal/config"
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	cfg := config.Load()
+
+	r := chi.NewRouter()
+
+	r.Get("/health", func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{ "status": "ok", "message": "FairShare API is running" }` + "\n"))
+	})
+
+	server := &http.Server{
+		Addr: fmt.Sprintf(":%s", cfg.AppPort),
+		Handler: r,
+		ReadTimeout: 15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout: 60 * time.Second,
+	}
+
+	log.Printf("Server started on port %s (env : %s)", cfg.AppPort, cfg.AppEnv)
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server failed to start %v", err)
+	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppEnv  string
+    AppPort string
+}
+
+func Load() *Config {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("Warning: .env file not found, failed to read environment variable")
+	}
+
+	return &Config {
+		AppEnv: getEnv("APP_ENV", "development"),
+		AppPort: getEnv("APP_PORT", "8080"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return fallback
+	}
+
+	return value
+}
